8bf7e068e01a65b9891988bf4f260ff51c5008ad
rakeshyadav1 <rakeshyadav1@gofynd.com>
2023-05-06 12:04:35 +0530

    initial setup

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  mode: "jit",
+  purge: ["./index.html", "./src/**/*.{js,jsx,ts,tsx,vue}"],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
